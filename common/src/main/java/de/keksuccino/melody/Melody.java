ee3f3ea1182dffd9d198a4cc1042b5a9041a2a64
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2023-12-27 03:12:44 +0100

    v1.0.1

--- common/src/main/java/de/keksuccino/melody/Melody.java
+++ common/src/main/java/de/keksuccino/melody/Melody.java
@@ -8,7 +8,7 @@ public class Melody {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "melody";
 
@@ -17,7 +17,7 @@ public class Melody {
 		if (Services.PLATFORM.isOnClient()) {
 			LOGGER.info("[MELODY] Loading Melody background audio library..");
 		} else {
-			LOGGER.error("[MELODY] Melody is a client-side audio library and does nothing when loaded on a server!");
+			LOGGER.info("[MELODY] Disabling Melody since it's loaded server-side.");
 		}
 
 	}
