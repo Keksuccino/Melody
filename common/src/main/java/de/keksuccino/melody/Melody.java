ad59d558751330e663441eaba710c7fdd16e1614
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-01-24 13:20:23 +0100

    ---

--- common/src/main/java/de/keksuccino/melody/Melody.java
+++ common/src/main/java/de/keksuccino/melody/Melody.java
@@ -8,7 +8,7 @@ public class Melody {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "melody";
 
