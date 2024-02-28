bac6653817172efe32c2b9ba0b938ca68315c97a
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-02-28 12:15:22 +0100

    ---

--- common/src/main/java/de/keksuccino/melody/Melody.java
+++ common/src/main/java/de/keksuccino/melody/Melody.java
@@ -8,14 +8,14 @@ public class Melody {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "melody";
 
 	public static void init() {
 
 		if (Services.PLATFORM.isOnClient()) {
-			LOGGER.info("[MELODY] Loading Melody background audio library..");
+			LOGGER.info("[MELODY] Loading v" + VERSION + " on " + MOD_LOADER.toUpperCase() + "...");
 		} else {
 			LOGGER.info("[MELODY] Disabling Melody since it's loaded server-side.");
 		}
---
+++ neoforge/src/main/java/de/keksuccino/melody/MelodyNeoForge.java
@@ -0,0 +1,14 @@
+package de.keksuccino.melody;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Melody.MOD_ID)
+public class MelodyNeoForge {
+    
+    public MelodyNeoForge() {
+
+        Melody.init();
+        
+    }
+
+}
