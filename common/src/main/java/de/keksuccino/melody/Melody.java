37d2a93d5365daff3c30b64608a170220d13c6a1
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2024-06-15 12:50:28 +0200

    ---

--- common/src/main/java/de/keksuccino/melody/Melody.java
+++ common/src/main/java/de/keksuccino/melody/Melody.java
@@ -8,7 +8,7 @@ public class Melody {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "1.0.9";
+	public static final String VERSION = "1.0.10";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "melody";
 
@@ -17,7 +17,7 @@ public class Melody {
 		if (Services.PLATFORM.isOnClient()) {
 			LOGGER.info("[MELODY] Loading v" + VERSION + " on " + MOD_LOADER.toUpperCase() + "...");
 		} else {
-			LOGGER.info("[MELODY] Disabling Melody since it's loaded server-side.");
+			LOGGER.warn("[MELODY] Disabling Melody since it's loaded server-side.");
 		}
 
 	}
