55d9ab97900b71675f94cfccbb3a7af16d2b0a8a
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-04-25 23:21:01 +0200

    v1.0.11

--- common/src/main/java/de/keksuccino/melody/Melody.java
+++ common/src/main/java/de/keksuccino/melody/Melody.java
@@ -8,7 +8,7 @@ public class Melody {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String VERSION = "1.0.10";
+	public static final String VERSION = "1.0.11";
 	public static final String MOD_LOADER = Services.PLATFORM.getPlatformName();
 	public static final String MOD_ID = "melody";
 
