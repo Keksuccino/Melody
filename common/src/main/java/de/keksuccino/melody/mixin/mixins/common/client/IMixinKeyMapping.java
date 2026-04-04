401fc86aea92463748bac52b74dd6779ec817b48
Keksuccino <no@mail.net>
2026-04-04 20:12:48 +0200

    Port to MC 26.1.1

---
+++ common/src/main/java/de/keksuccino/melody/mixin/mixins/common/client/IMixinKeyMapping.java
@@ -0,0 +1,14 @@
+package de.keksuccino.melody.mixin.mixins.common.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(KeyMapping.class)
+public interface IMixinKeyMapping {
+
+    @Accessor("key")
+    InputConstants.Key get_key_Melody();
+
+}
--- fabric/src/main/java/de/keksuccino/melody/platform/FabricPlatformHelper.java
+++ fabric/src/main/java/de/keksuccino/melody/platform/FabricPlatformHelper.java
@@ -1,9 +1,9 @@
 package de.keksuccino.melody.platform;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import de.keksuccino.melody.mixin.mixins.common.client.IMixinKeyMapping;
 import de.keksuccino.melody.platform.services.IPlatformHelper;
 import net.fabricmc.api.EnvType;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.client.KeyMapping;
@@ -68,7 +68,7 @@ public class FabricPlatformHelper implements IPlatformHelper {
 
     @Override
     public InputConstants.Key getKeyMappingKey(KeyMapping keyMapping) {
-        return KeyBindingHelper.getBoundKeyOf(keyMapping);
+        return ((IMixinKeyMapping) keyMapping).get_key_Melody();
     }
 
 }
